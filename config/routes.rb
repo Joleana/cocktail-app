c7f5b60ea8ab9039be88220360ab38e04d79fe1d
Joleana Shurley <joleanashurley@gmail.com>
2021-02-18 15:57:13 +0100

    add models and set validations

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :cocktails, only: [:index, :new, :create, :show] do
+    resources :ingredients, only: [:new, :create]
+    resources :doses, only: [:new, :create]
+  end
+  root to: 'cocktails#index'
 end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Ingredient.create(name: "mint")
+Ingredient.create(name: "vodka")
+Ingredient.create(name: "rhum")
+Ingredient.create(name: "tequila")
+Ingredient.create(name: "orange juice")
+Ingredient.create(name: "tomato juice")
+
+Cocktail.create(name: "mojito")
+Cocktail.create(name: "bloody mary")
+Cocktail.create(name: "sex on the beach")
+Cocktail.create(name: "margarita")
+Cocktail.create(name: "pina colada")
+
+Dose.create(description: "6 leaves", cocktail_id: 1, ingredient_id: 1)
+Dose.create(description: "1.5 oz", cocktail_id: 1, ingredient_id: 3)
+Dose.create(description: "4.5 cl", cocktail_id: 2, ingredient_id: 2)
+Dose.create(description: "9 cl", cocktail_id: 2, ingredient_id: 6)
+Dose.create(description: "1.33 oz", cocktail_id: 3, ingredient_id: 2)
+Dose.create(description: "1.33 oz", cocktail_id: 3, ingredient_id: 5)
+Dose.create(description: "2 cl", cocktail_id: 4, ingredient_id: 4)
